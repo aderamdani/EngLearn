86aad691ce0963ea672c6b8f595c3358044de2ce
Ade Ramdani <mr.aderamdani@gmail.com>
2026-06-07 16:06:50 +0700

    Initial Commit

---
+++ EngLearnTests/EngLearnTests.swift
@@ -0,0 +1,19 @@
+//
+//  EngLearnTests.swift
+//  EngLearnTests
+//
+//  Created by Ade Ramdani on 07/06/26.
+//
+
+import Testing
+@testable import EngLearn
+
+struct EngLearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
